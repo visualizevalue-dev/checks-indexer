514126098be58fe95b75a8e88f143e750589a19f
jalil <jalil@1001.digital>
2024-03-06 15:24:12 +0100

    Add transfer + newEpoch indexer

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,8 +1,25 @@
 import { createSchema } from "@ponder/core";
 
 export default createSchema((p) => ({
-  Example: p.createTable({
+  Account: p.createTable({
     id: p.string(),
-    name: p.string().optional(),
   }),
-}));
+  Check: p.createTable({
+    id: p.bigint(),
+    ownerId: p.string().references('Account.id'),
+    owner: p.one('ownerId'),
+    seed: p.string().optional(),
+    isRevealed: p.boolean(),
+    checksCount: p.int().optional(),
+    svg: p.string().optional(),
+    epoch: p.int(),
+    gradient: p.int().optional(),
+    colorBand: p.int().optional(),
+    direction: p.int().optional(),
+    speed: p.int().optional(),
+    composite: p.string().optional(),
+    composites: p.string().list().optional(),
+    migratedAt: p.bigint(),
+    revealedAt: p.bigint().optional(),
+  }),
+}))
--- src/Checks.ts
+++ src/Checks.ts
@@ -1,17 +1,61 @@
-import { ponder } from "@/generated";
+import { ponder } from '@/generated'
 
-ponder.on("Checks:Approval", async ({ event, context }) => {
-  console.log(event.args);
-});
+ponder.on('Checks:Transfer', async ({ event, context }) => {
+  const { client } = context
+  const { Checks } = context.contracts
+  const { Check, Account } = context.db
 
-ponder.on("Checks:ApprovalForAll", async ({ event, context }) => {
-  console.log(event.args);
-});
+  await Promise.all([
+    await Account.upsert({ id: event.args.from }),
+    await Account.upsert({ id: event.args.to }),
+  ])
 
-ponder.on("Checks:Composite", async ({ event, context }) => {
-  console.log(event.args);
-});
+  const check = await client.readContract({
+    abi: Checks.abi,
+    address: Checks.address,
+    functionName: `getCheck`,
+    args: [event.args.tokenId],
+  })
 
-ponder.on("Checks:Infinity", async ({ event, context }) => {
-  console.log(event.args);
-});
+  await Check.upsert({
+    id: event.args.tokenId,
+    create: {
+      ownerId: event.args.to,
+      migratedAt: event.block.timestamp,
+      epoch: check.stored.epoch,
+      isRevealed: check.isRevealed,
+    },
+    update: {
+      ownerId: event.args.to,
+    }
+  })
+})
+
+ponder.on('Checks:NewEpoch', async ({ event, context }) => {
+  const { client } = context
+  const { Checks } = context.contracts
+  const { Check } = context.db
+
+  const checksInEpoch = await Check.findMany({
+    where: {
+      epoch: Number(event.args.epoch),
+    }
+  })
+
+  for (const check of checksInEpoch.items) {
+    const svg = await client.readContract({
+      abi: Checks.abi,
+      address: Checks.address,
+      functionName: `svg`,
+      args: [check.id],
+      blockNumber: event.args.revealBlock,
+    })
+
+    await Check.update({
+      id: check.id,
+      data: {
+        svg
+      }
+    })
+  }
+})
